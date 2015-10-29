32bd8b68691c3292341d1582a49021f5adac8083
jungy184 <jungy184@newschool.edu>
2015-10-29 23:49:11 +0000

    js

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("hello world");
+console.log("*************");
+
+var symbols = ["*","$","#","@","*","$","#","@","*","$","#","@","*","$","#","@"]
+
+var pattern = "*";
+
+for (var increment = 0; increment < symbols.length; increment++) {
+    console.log(pattern);
+    pattern = pattern + symbols[increment];
+    var ampersand = "&";
+    console.log(increment);
+    if (increment > 5) {
+        for (var another = 0; another < 10; another++) {
+            console.log(ampersand);
+            ampersand = ampersand + "&";
+        }
+    }
+}
+
+/*var symbols1 = [];
+var symbols2 = [];
+
+var pattern1 = "*";
+var pattern2= "-";
+
+for (var i = 0; i <= 9; i++) {
+        symbols1.push(pattern1);
+        pattern1 = pattern1 + "*";
+}
+for (var i = 0; i <= 9; i++) {
+        symbols2.push(pattern2);
+        pattern2 = pattern2 + "-";
+}
+symbols2.reverse();
+
+for (var i=0; i<=9; i++){
+    console.log(symbols1[i],symbols2[i],symbols1[i],symbols2[i],symbols1[i],symbols2[i],symbols1[i],symbols2[i]);
+}
+
+var runningAmp
+
+var runningAp = function() {
+    for (var line = "#"; line.length < 8; line =+ "#") {
+        console.log(line)
+    }
+}
---
+++ script2.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+function myFunction() {
+    //do stuff
+    console.log("hello world")
+}
+
+function addNums(num1,num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+$("body").css("background-color","blue")
